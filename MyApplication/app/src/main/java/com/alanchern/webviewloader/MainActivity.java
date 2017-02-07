62eb8aaba23caf7f3a2ed958c2fcc56974a4894c
Alan Chern <alan.chern@bluefletch.com>
2017-02-07 10:12:34 -0500

    initial commit

---
+++ MyApplication/app/src/main/java/com/alanchern/webviewloader/MainActivity.java
@@ -0,0 +1,49 @@
+package com.alanchern.webviewloader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.loadUrl("file:///android_asset/floor.html");
+    }
+
+    private void init() {
+        // make app full screen
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        getWindow().getDecorView().setSystemUiVisibility(flags);
+
+        // prevent volume buttons from exiting full screen
+        final View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    decorView.setSystemUiVisibility(flags);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        // super.onBackPressed();
+    }
+}
